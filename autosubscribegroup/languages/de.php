2bba426baf102c1866c639707a9ab50a9fcd0f93
iionly <iionly@gmx.de>
2013-11-12 20:55:53 +0100

    Initial releas for Elgg 1.9; see README.txt

---
+++ autosubscribegroup/languages/de.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+  'autosubscribe:list' => "Gruppen-IDs (getrennt durch Kommas): "
+);
---
+++ autosubscribegroup/start.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Elgg autosubscribegroup plugin
+ * This plugin allows new users to get joined to groups automatically when they register.
+ *
+ * @package autosubscribegroups
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author RONNEL Jérémy
+ * @copyright (c) Elbee 2008-2013
+ * @link /www.notredeco.com
+ *
+ * for Elgg 1.9 by iionly (iionly@gmx.de)
+ */
+
+/**
+ * Init
+ *
+ */
+elgg_register_event_handler('init', 'system', 'autosubscribegroup_init');
+
+function autosubscribegroup_init() {
+    // Listen to user registration
+    elgg_register_event_handler('create', 'user', 'autosubscribegroup_join', 502);
+}
+
+/**
+ * auto join group define in plugin settings
+ *
+ */
+function autosubscribegroup_join($event, $object_type, $object) {
+
+    if (($object instanceof ElggUser) && ($event == 'create') && ($object_type == 'user')) {
+        //auto submit relationships between user & groups
+        //retrieve groups ids from plugin
+        $groups = elgg_get_plugin_setting('systemgroups', 'autosubscribegroup');
+        $groups = split(',', $groups);
+
+        //for each group ids
+        foreach($groups as $groupId) {
+            //if group exist : submit to group
+            if ($groupEnt = get_entity($groupId)) {
+                //join group succeed?
+                if ($groupEnt->join($object)) {
+                    // Remove any invite or join request flags
+                    elgg_delete_metadata(array('guid' => $object->guid, 'metadata_name' => 'group_invite', 'metadata_value' => $groupEnt->guid, 'limit' => false));
+                    elgg_delete_metadata(array('guid' => $object->guid, 'metadata_name' => 'group_join_request', 'metadata_value' => $groupEnt->guid, 'limit' => false));
+                }
+            }
+        }
+    }
+}
